338b157cc6de33485a729cee1d770fac9a23fc91
lanzmoncis <lanzmoncis@gmail.com>
2023-04-13 14:19:34 +0100

    First commit

---
+++ controllers/tourController.js
@@ -0,0 +1,91 @@
+const Tour = require('./../models/tourModel');
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTourById = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findByOne({ _id: req.params.id})
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
+};
+
+exports.patchTourById = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.deleteTourById = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    return res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'WAY SWERTE BRAD WAY USER',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'WAY SWERTE BRAD WAY USER',
+  });
+};
+
+exports.getUserById = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'WAY SWERTE BRAD WAY USER',
+  });
+};
+
+exports.patchUserById = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'WAY SWERTE BRAD WAY USER',
+  });
+};
+
+exports.deleteUseById = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'WAY SWERTE BRAD WAY USER',
+  });
+};
